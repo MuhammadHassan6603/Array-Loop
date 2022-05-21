75c9d2f1b808d2a8111a60fce47ef93ba70c42b8
Muhammad Hassan <105521572+MuhammadHassan6603@users.noreply.github.com>
2022-05-21 16:23:14 +0500

    Add files via upload

---
+++ ArrayLoop.java
@@ -0,0 +1,13 @@
+public class ArrayLoop
+{
+	public static void main(String args[])
+	{
+  
+		int[] age = {12, 4, 5, 9, 15, 26, 31, 18, 20};
+		System.out.println("Using for Loop:");
+		for(int i = 0; i < 8 ; i++) 
+			{
+				System.out.println(age[i]);
+			}
+	}
+}
